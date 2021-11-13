3bc0f40b5f4ad8b0f8822d2bbcf244c9426d8955
ANSIoso <92176296+ANSIoso@users.noreply.github.com>
2021-11-13 20:27:31 +0100

    buh

--- Mirinamuni/src/application/Main.java
+++ Mirinamuni/src/application/Main.java
@@ -7,8 +7,6 @@ import application.controller.SceneHandler;
 public class Main {
 	public static void main(String[] args) {
 		
-		System.out.println("ciao");
-		
 		JFrame frame = new JFrame();
 		
 		SceneHandler.getInstance().init(frame);
